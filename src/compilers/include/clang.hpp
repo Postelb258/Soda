7dedc55d15c3624e1660e333273fa7aadf94aae4
Postelb <clewith@gmail.com>
2023-11-11 17:06:24 +0400

    temporarily build mode removed

--- src/compilers/include/clang.hpp
+++ src/compilers/include/clang.hpp
@@ -2,20 +2,22 @@
 #define __CLANG_COMPILER_H
 
 #include <iostream>
+#include <stdexcept>
+
 #include "../../api/include/fs.hpp"
 #include "../../api/include/shell.hpp"
-#include "../../core/include/config.hpp"
 #include "../../core/include/compilation.hpp"
+#include "../../core/include/config.hpp"
 
 class Clang : public CompilationStrategy {
-private:
-    BuildMode m_buildmode;
-    std::unique_ptr<Config> m_config;
-public:
-    Clang(std::unique_ptr<Config> config, BuildMode build_mode);
-    void build() override;
-    void link() override;
-    void run() override; 
+ private:
+  std::unique_ptr<Config> m_config;
+
+ public:
+  Clang(std::unique_ptr<Config> config);
+  void build() override;
+  void link() override;
+  void run() override;
 };
 
 #endif
--- src/core/include/compilation.hpp
+++ src/core/include/compilation.hpp
@@ -1,23 +1,18 @@
 #ifndef __COMPILATION_H
 #define __COMPILATION_H
 
-enum BuildMode {
-    debug,
-    release
-};
-
 class LinkStrategy {
-public:
-    virtual void makeExecutable() = 0;
-    virtual void makeStaticLibrary() = 0;
-    virtual void makeSharedLibrary() = 0;
+ public:
+  virtual void makeExecutable() = 0;
+  virtual void makeStaticLibrary() = 0;
+  virtual void makeSharedLibrary() = 0;
 };
 
 class CompilationStrategy {
-public: 
-    virtual void build() = 0;
-    virtual void link() = 0;
-    virtual void run() = 0;
+ public:
+  virtual void build() = 0;
+  virtual void link() = 0;
+  virtual void run() = 0;
 };
 
 #endif
