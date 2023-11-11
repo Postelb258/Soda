81e31eb5284dcf9dafa7c31fa05e930c797d297f
Postelb <clewith@gmail.com>
2023-11-11 17:07:27 +0400

    build mode is removed temporarily

--- src/subcommands/build.cpp
+++ src/subcommands/build.cpp
@@ -1,17 +1,15 @@
 #include "include/build.hpp"
 
-Build::Build(std::unique_ptr<Config> config) : m_config(std::move(config)), m_buildmode(BuildMode::debug) {}
+Build::Build(std::unique_ptr<Config> config) : m_config(std::move(config)) {}
 
 CLI::App* Build::setup(CLI::App& app) {
   CLI::App* sub = app.add_subcommand("build", "builds a project");
-  bool release{false};
-  sub->add_flag("-r, --release", release, "Builds a release");
-  this->m_buildmode = release ? BuildMode::release : BuildMode::debug;
+
   return sub;
 }
 
 void Build::handle() {
-  Clang* clang = new Clang(std::move(this->m_config), this->m_buildmode);
+  Clang* clang = new Clang(std::move(this->m_config));
   clang->build();
   free(clang);
 }
--- src/subcommands/include/build.hpp
+++ src/subcommands/include/build.hpp
@@ -1,18 +1,18 @@
 #ifndef __BUILD_SUBCMD_H
 #define __BUILD_SUBCMD_H
 
+#include "../../compilers/include/clang.hpp"
 #include "../../core/include/cli.hpp"
 #include "../../core/include/config.hpp"
-#include "../../compilers/include/clang.hpp"
 
 class Build : public Subcommand {
-private: 
-    BuildMode m_buildmode;
-    std::unique_ptr<Config> m_config;
-public:
-    Build(std::unique_ptr<Config> config);
-    CLI::App* setup(CLI::App& app) override;
-    void handle() override;
+ private:
+  std::unique_ptr<Config> m_config;
+
+ public:
+  Build(std::unique_ptr<Config> config);
+  CLI::App* setup(CLI::App& app) override;
+  void handle() override;
 };
 
 #endif
