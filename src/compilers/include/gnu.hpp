e89570e0a1cbdf036b467fd6ae9506fddb4e7698
Postelb <clewith@gmail.com>
2023-11-11 23:09:11 +0400

    Initial commit

--- src/compilers/include/gnu.hpp
+++ src/compilers/include/gnu.hpp
@@ -1,4 +1,21 @@
 #ifndef __GNU_COMPILER_H
 #define __GNU_COMPILER_H
 
+#include "../../api/include/fs.hpp"
+#include "../../api/include/shell.hpp"
+#include "../../core/include/compilation.hpp"
+#include "../../core/include/config.hpp"
+
+class GNU : public CompilationStrategy {
+ private:
+  std::unique_ptr<Config> m_config;
+  BuildMode m_mode;
+
+ public:
+  GNU(std::unique_ptr<Config> config, BuildMode build_mode);
+  void build() override;
+  void link() override;
+  void run() override;
+};
+
 #endif
