2d50b1e57f3f0f1032a36184f57b7a85131b2e1c
Postelb <clewith@gmail.com>
2023-11-11 18:31:03 +0400

    optimization

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -1,6 +1,6 @@
 #include "include/config.hpp"
 
-Package::Package(const BasicValue &config, const BasicValue &package)
+Package::Package(const BasicValue &package)
     : name(toml::find<std::string_view>(package, "name")),
       entry(toml::find<std::string_view>(package, "entry")),
       version(toml::find<std::string_view>(package, "version")) {
@@ -9,26 +9,23 @@ Package::Package(const BasicValue &config, const BasicValue &package)
   }
 }
 
-std::optional<Lib> Lib::constructOptionally(const BasicValue &config,
-                                            const BasicValue &lib) {
+std::optional<Lib> Lib::constructOptionally(const BasicValue &lib) {
   std::optional<Lib> s_lib;
-  if (config.contains("lib")) s_lib = Lib{};
+  if (lib.is_table()) s_lib = Lib{};
 
   return s_lib;
 }
 
-std::optional<Debug> Debug::constructOptionally(const BasicValue &config,
-                                                const BasicValue &debug) {
+std::optional<Debug> Debug::constructOptionally(const BasicValue &debug) {
   std::optional<Debug> s_debug;
-  if (config.contains("debug")) s_debug = Debug{};
+  if (debug.is_table()) s_debug = Debug{};
 
   return s_debug;
 }
 
-std::optional<Release> Release::constructOptionally(const BasicValue &config,
-                                                    const BasicValue &release) {
+std::optional<Release> Release::constructOptionally(const BasicValue &release) {
   std::optional<Release> s_release;
-  if (config.contains("release")) {
+  if (release.is_table()) {
     uint8_t optimization = toml::find_or(release, "optimization", 2);
     s_release = Release{.optimization = optimization};
   }
@@ -37,9 +34,9 @@ std::optional<Release> Release::constructOptionally(const BasicValue &config,
 }
 
 std::optional<Dependencies> Dependencies::constructOptionally(
-    const BasicValue &config, const BasicValue &dependencies) {
+    const BasicValue &dependencies) {
   std::optional<Dependencies> s_dependencies;
-  if (config.contains("dependencies")) {
+  if (dependencies.is_table()) {
     auto dependencies_map =
         toml::find_or<std::pair<std::string_view, std::string_view>>(
             dependencies, "dependencies", {});
@@ -50,25 +47,25 @@ std::optional<Dependencies> Dependencies::constructOptionally(
 }
 
 std::optional<Config> Config::load(const std::string &path) {
-  std::optional<Config> s_config;
+  Config s_config;
   struct stat info;
 
-  if (stat(path.c_str(), &info) != 0) return s_config;
+  if (stat(path.c_str(), &info) != 0) {
+    return std::optional<Config>();
+  };
 
   auto config = toml::parse(path);
+  s_config.package = Package(toml::find<BasicValue>(config, "package"));
+  s_config.lib =
+      Lib::constructOptionally(toml::find_or<BasicValue>(config, "lib", {}));
+  s_config.debug = Debug::constructOptionally(
+      toml::find_or<BasicValue>(config, "debug", {}));
+  s_config.release = Release::constructOptionally(
+      toml::find_or<BasicValue>(config, "release", {}));
+  s_config.dependencies = Dependencies::constructOptionally(
+      toml::find_or<BasicValue>(config, "dependencies", {}));
 
-  s_config->package =
-      Package(config, toml::find<BasicValue>(config, "package"));
-  s_config->lib =
-      Lib::constructOptionally(config, toml::find<BasicValue>(config, "lib"));
-  s_config->debug = Debug::constructOptionally(
-      config, toml::find<BasicValue>(config, "debug"));
-  s_config->release = Release::constructOptionally(
-      config, toml::find<BasicValue>(config, "release"));
-  s_config->dependencies = Dependencies::constructOptionally(
-      config, toml::find<BasicValue>(config, "dependencies"));
-
-  return s_config;
+  return std::optional<Config>(s_config);
 }
 
 std::unique_ptr<Config> Config::getLoaded(
