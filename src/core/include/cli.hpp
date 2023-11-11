b5a3fdc3d041c4f4f028283d403c5a0dcdf6123c
Postelb <clewith@gmail.com>
2023-11-11 17:06:58 +0400

    Initial commit

--- src/core/include/cli.hpp
+++ src/core/include/cli.hpp
@@ -1,20 +1,21 @@
-#ifndef __CLI_H 
-#define __CLI_H 
+#ifndef __CLI_H
+#define __CLI_H
 
 #include <CLI11.hpp>
 
 class Subcommand {
-public:
-    virtual CLI::App* setup(CLI::App& app) = 0;
-    virtual void handle() = 0;
+ public:
+  virtual CLI::App* setup(CLI::App& app) = 0;
+  virtual void handle() = 0;
 };
 
 class CLIApp {
-private:
-    std::unique_ptr<CLI::App> m_app;
-public:
-    CLIApp(std::vector<std::shared_ptr<Subcommand>>& subcommands);
-    int run(int argc, char** argv);
+ private:
+  std::unique_ptr<CLI::App> m_app;
+
+ public:
+  CLIApp(std::vector<std::shared_ptr<Subcommand>>& subcommands);
+  int run(int argc, char** argv);
 };
 
 #endif
