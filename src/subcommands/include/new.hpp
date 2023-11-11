db23c1a0727d9fc35617f8c55f6ff26c8f5f8379
Postelb <clewith@gmail.com>
2023-11-11 17:07:40 +0400

    .hpp refactor

--- src/subcommands/include/new.hpp
+++ src/subcommands/include/new.hpp
@@ -1,22 +1,25 @@
 #ifndef __NEW_SUBCMD_H
 #define __NEW_SUBCMD_H
 
-#include "../../core/include/cli.hpp"
-#include <stdexcept>
-#include <system_error>
-#include <fstream>
 #include <bits/stdc++.h>
 #include <sys/stat.h>
 
+#include <fstream>
+#include <stdexcept>
+#include <system_error>
+
+#include "../../core/include/cli.hpp"
+
 class New : public Subcommand {
-private:
-    std::string m_path;
-    bool m_lib;
-    bool m_cxx;
-public:
-    New();
-    CLI::App* setup(CLI::App& app) override;
-    void handle() override;
+ private:
+  std::string m_path;
+  bool m_lib;
+  bool m_cxx;
+
+ public:
+  New();
+  CLI::App* setup(CLI::App& app) override;
+  void handle() override;
 };
 
 #endif
