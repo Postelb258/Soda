97b69e9281c7a03537d6f6c52c1ca13ec5930bba
Postelb <clewith@gmail.com>
2023-11-11 12:22:27 +0400

    Add unique ptr to Config

--- src/compilers/clang.cpp
+++ src/compilers/clang.cpp
@@ -1,1 +1,32 @@
 #include "include/clang.hpp"
+#include <stdexcept>
+
+Clang::Clang(std::unique_ptr<Config> config) : m_config(std::move(config)) {}
+
+void Clang::build() {
+    if (this->m_config == nullptr)
+        throw std::runtime_error("Config file is not detected");
+
+    
+}
+
+// LinkStrategy Clang::link() {
+    
+// }
+
+void Clang::run() {
+
+}
+
+void Clang::makeExecutable() {
+
+}
+
+void Clang::makeStaticLibrary() {
+
+}
+
+void Clang::makeSharedLibrary() {
+
+}
+
--- src/compilers/include/clang.hpp
+++ src/compilers/include/clang.hpp
@@ -1,10 +1,14 @@
 #ifndef __CLANG_COMPILER_H
 #define __CLANG_COMPILER_H
 
+#include "../../core/include/config.hpp"
 #include "../../core/include/compilation.hpp"
 
 class Clang : public CompilationStrategy, public LinkStrategy {
+private:
+    std::unique_ptr<Config> m_config;
 public:
+    Clang(std::unique_ptr<Config> config);
     void build() override;
     LinkStrategy link() override;
     void run() override; 
