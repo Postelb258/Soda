6ead0e57c59dc95d352576f9ecdf7b836e9c274a
Postelb <clewith@gmail.com>
2023-11-11 18:29:48 +0400

    add `build` subcommand

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include "core/include/cli.hpp"
 #include "core/include/compilation.hpp"
 #include "core/include/config.hpp"
-#include "subcommands/include/new.hpp"
 #include "subcommands/include/build.hpp"
+#include "subcommands/include/new.hpp"
 
 int main(int argc, char **argv) {
-  std::unique_ptr<Config> config = Config::getLoaded(Config::load("Soda.toml"));
+  std::unique_ptr<Config> config =
+      Config::getLoaded(Config::load("./Soda.toml"));
 
-  std::vector<std::shared_ptr<Subcommand>> subcmds{std::make_shared<New>(), std::make_shared<Build>()};
+  std::vector<std::shared_ptr<Subcommand>> subcmds{
+      std::make_shared<New>(), std::make_shared<Build>(std::move(config))};
   std::unique_ptr<CLIApp> app = std::make_unique<CLIApp>(subcmds);
   app->run(argc, argv);
 }
