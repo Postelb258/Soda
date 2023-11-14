f8bb300d81be7a15f709cadb5de86cf275214190
Postelb <clewith@gmail.com>
2023-11-14 21:40:49 +0400

    ?

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include "core/include/config.hpp"
 #include "subcommands/include/build.hpp"
 #include "subcommands/include/new.hpp"
+#include "subcommands/include/test.hpp"
 
 int main(int argc, char **argv) {
   std::unique_ptr<Config> config = Config::load("./Soda.toml");
 
   std::vector<std::shared_ptr<Subcommand>> subcmds{
-      std::make_shared<New>(), std::make_shared<Build>(std::move(config))};
+      std::make_shared<New>(), std::make_shared<Build>(std::move(config)),
+      std::make_shared<Test>()};
   std::unique_ptr<CLIApp> app = std::make_unique<CLIApp>(subcmds);
   app->run(argc, argv);
 }
