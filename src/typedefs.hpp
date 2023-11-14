723bfb00ae9cf3bbd3ba8e4411ccdc88e95d2d29
Postelb <clewith@gmail.com>
2023-11-14 23:43:45 +0400

    Add typedefs

--- src/typedefs.hpp
+++ src/typedefs.hpp
@@ -1,6 +1,7 @@
 #ifndef __TYPEDEFS_H
 #define __TYPEDEFS_H
 
+#include <filesystem>
 #include <map>
 #include <optional>
 #include <toml.hpp>
@@ -9,10 +10,15 @@ typedef toml::basic_value<toml::discard_comments, std::unordered_map,
                           std::vector>
     BasicValue;
 
+typedef std::string str;
 typedef std::string_view sview;
+typedef std::filesystem::path fs_path;
 typedef std::map<std::string_view, std::string_view> smap;
 
 template <typename T>
 using opt = std::optional<T>;
+
+template <typename T>
+using vec = std::vector<T>;
 
 #endif
