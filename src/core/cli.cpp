63c5ea1f714e9a7e51c0fa8fee6793c74dc12685
Postelb <clewith@gmail.com>
2023-11-17 23:18:57 +0400

    pass in callback not by &

--- src/core/cli.cpp
+++ src/core/cli.cpp
@@ -1,15 +1,15 @@
 #include "include/cli.hpp"
 
 CLIApp::CLIApp(vec<std::shared_ptr<Subcommand>> subcommands)
-    : m_app(new CLI::App()) {
+    : m_app(std::make_unique<CLI::App>()) {
   for (auto &subcommand : subcommands) {
     CLI::App *subcmd = subcommand->setup(*this->m_app);
-    subcmd->callback([&]() { subcommand->handle(); });
+    subcmd->callback([subcommand]() { subcommand->handle(); });
   }
   this->m_app->require_subcommand();
 }
 
 int CLIApp::run(int argc, char **argv) {
-  CLI11_PARSE(*this->m_app, argc, argv);
+  CLI11_PARSE(*(this->m_app), argc, argv);
   return 0;
 }
