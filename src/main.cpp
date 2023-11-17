edda97d4027ced537959a59a58329248ca01683a
Postelb <clewith@gmail.com>
2023-11-17 20:22:37 +0400

    `unique` -> `shared` pointer

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 #include "subcommands/include/new.hpp"
 
 int main(int argc, char **argv) {
-  std::unique_ptr<Config> config = Config::load("./Soda.toml");
+  std::shared_ptr<Config> config = Config::load("./Soda.toml");
 
-  std::vector<std::unique_ptr<Subcommand>> subcmds{
-      std::make_unique<New>(), std::make_unique<Build>(std::move(config))};
+  vec<std::shared_ptr<Subcommand>> subcmds = {std::make_shared<New>(),
+                                              std::make_shared<Build>(config)};
   std::unique_ptr<CLIApp> app = std::make_unique<CLIApp>(subcmds);
   app->run(argc, argv);
 }
